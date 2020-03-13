c3573985a6a3e0aaed8209556a2a02126a98e4fa
illumiMatii <mateuszurbanek102@gmail.com>
2020-03-13 20:50:40 +0100

    Task 1 complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-/* Here goes your JS code */
+document.addEventListener("DOMContentLoaded", function(){
+    showBtn.addEventListener("click", function(){
+        loginForm.style.display = "block";
+        showBtn.style.display = "none";  
+    });
+
+    loginForm.addEventListener("click", (e) =>{
+        if(e.target.closest("#closebtn") !== null){
+            loginForm.style.display = "none";
+            showBtn.style.display = "block";
+        }
+    });
+});
+
+
+const showBtn = document.querySelector("#show-popup-form");
+const loginForm = document.querySelector(".popup");
+const closeBtn = document.querySelector("#closebtn");
